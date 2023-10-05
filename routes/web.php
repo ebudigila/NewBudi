7583c00334f06e4e9bd1f91166a6aab51c7dc56f
Emmanuel Budigila-ad <emmanuel.budigila-ad@msd.go.tz>
2023-10-05 15:05:09 +0300

    Update

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/', function () {
+    return view('welcome');
+});
 
